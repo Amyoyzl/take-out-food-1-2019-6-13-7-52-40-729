4e952f4a513a198bfd5427077ec1cefa217d82cd
Amyoyzl <1421790940@qq.com>
2019-07-11 00:55:42 +0800

    implements bestCharge

--- src/best-charge.js
+++ src/best-charge.js
@@ -22,7 +22,35 @@ const getItemsInfo = (items, allItems) => {
 }
 
 const bestCharge = selectedItems => {
-  // TODO 
+  const countItems = getItems(selectedItems);
+  const allItems = dateItem.loadAllItems();
+  const items = getItemsInfo(countItems, allItems);
+  const promotions = dataPromotion.loadPromotions().slice();
+  let receipt = "============= 订餐明细 =============\n";
+  let total = 0;
+  let bestSave = 0;
+  items.forEach(item => {
+    receipt += `${item.name} x ${item.count} = ${item.price * item.count}元\n`;
+    total += item.price * item.count;
+    promotions[1].items.forEach(e => {
+      if (e == item.id) bestSave += item.price / 2;
+    })
+  })
+  if (bestSave > 0) {
+    receipt += "-----------------------------------\n使用优惠:\n";
+    let saveString = "";
+    if (total >= 30 && bestSave < 6) {
+      bestSave = 6;
+      saveString = `满30减6元，省6元\n`;
+    }
+    if (bestSave > 6) {
+      saveString = `指定菜品半价(黄焖鸡，凉皮)，省${bestSave}元\n`;
+    }
+    receipt += saveString;
+  }
+  receipt += "-----------------------------------\n";
+  receipt += `总计：${total - bestSave}元\n===================================`;
+  return receipt;
 }
 
 module.exports = {
